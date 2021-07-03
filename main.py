335a8ba0d946327cc819361751bda9448e9cf6c7
Owen Quinlan <46948241+BuyMyMojo@users.noreply.github.com>
2021-07-03 18:21:30 +1000

    moved to working with discord.ext.commands

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 import discord
+from discord.ext import commands
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+bot = commands.Bot(command_prefix='$')
 
-client = MyClient()
-client.run('ODYwNzg5MDAwMDYxODQ1NTE0.YOAWOg.ThcPFf4dg1v7IJrOH7n2GrueNiw')
+@bot.event
+async def on_ready():
+    print(f"Bot is ready")
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name="Using EAX on images"))
+    
+    
+# This command sends back the arguments given as one message
+@bot.command()
+async def foo(ctx, *, arg):
+    await ctx.send(arg)
+    
+# simple addition, adds two numbers together
+@bot.command()
+async def add(ctx, a: int, b: int):
+    await ctx.send(a + b)
+
+# simple addition, adds two numbers together but done in a seperate function
+@bot.command()
+async def funcadd(ctx, a: int, b: int):
+    await ctx.send(adding(a, b))
+
+def adding(a: int, b: int):
+    return(a+b)
+    
+        
+bot.run('ODYwNzg5MDAwMDYxODQ1NTE0.YOAWOg.ThcPFf4dg1v7IJrOH7n2GrueNiw')
