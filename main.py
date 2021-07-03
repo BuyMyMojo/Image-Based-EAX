0b5cc1ca3d4f100f9c206cc08f06d818dcecdc4d
Owen Quinlan <46948241+BuyMyMojo@users.noreply.github.com>
2021-07-03 19:04:22 +1000

    added ImgDif Command

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import discord
 from discord.ext import commands
+import requests
+import cv2 as cv
+from PIL import Image as pil_img
+from PIL import ImageChops as pil_img_chops
+import os
+
 
 
 bot = commands.Bot(command_prefix='$')
@@ -27,6 +33,43 @@ async def funcadd(ctx, a: int, b: int):
 
 def adding(a: int, b: int):
     return(a+b)
+
+@bot.command()
+async def ImgDif(ctx, img1: str, img2: str):
+    download_temp_image(img1, "dif_img_1")
+    download_temp_image(img2, "dif_img_2")
+    
+    get_img_dif("dif_img_1", "dif_img_2")
+    
+    delete_temp_file("dif_img_1")
+    delete_temp_file("dif_img_2")
+    
+    await ctx.send(file=discord.File('./temp/difference.png'))
+    
+    delete_temp_file("difference.png")
+        
+def download_temp_image(img, name):
+    response = requests.get(img)
+    file = open("./temp/" + name, "wb")
+    file.write(response.content)
+    file.close()
+    
+def delete_temp_file(name):
+    os.remove("./temp/"+ name)
+
+def get_img_dif(name1, name2):
+    imgA = pil_img.open("./temp/" + name1)
+    imgB = pil_img.open("./temp/" + name2)
+    
+    imgA_convert = imgA.convert("RGB")
+    imgB_convert = imgB.convert("RGB")
+    
+    difference = pil_img_chops.difference(imgA_convert, imgB_convert)
+    difference.save("./temp/difference.png")
+    
+    
+    
+    
     
         
 bot.run('ODYwNzg5MDAwMDYxODQ1NTE0.YOAWOg.ThcPFf4dg1v7IJrOH7n2GrueNiw')
